7a7dc4c056303a7d685b8d7dedf1902d06667dbd
Jan-Paul Kleemans <jpkleemans@gmail.com>
2016-04-30 16:37:54 +0200

    Rename interface

--- src/RegistersContainerEntries.php
+++ src/RegistersContainerEntries.php
@@ -3,7 +3,7 @@ declare(strict_types = 1);
 
 namespace Relevo\Container;
 
-interface ContainerEntries
+interface RegistersContainerEntries
 {
     public function registerContainerEntries(ContainerBuilder $container);
 }
